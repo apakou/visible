7db00424b36ef9fa72ffc86146497ffe815e29f7
FaWayo <wayofatima@gmail.com>
2026-03-22 23:31:36 +0000

    db error fix

--- app/DB/database.py
+++ app/DB/database.py
@@ -11,7 +11,7 @@ database_url = os.getenv("DATABASE_URL")
 if database_url is None:
     raise ValueError("DATABASE_URL environment variable not set")
 
-engine = create_engine(database_url, echo=True)
+engine = create_engine(database_url, echo=True, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
